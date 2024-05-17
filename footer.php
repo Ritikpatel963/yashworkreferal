27761c3faf966bac93f4d9838a74f765b84ed16d
unknown <rp0944467@gmail.com>
2024-05-17 22:14:45 +0530

    Your commit message here

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col">
+                <a href="#">
+                    <div class="icon"><i class="fa-solid fa-house" ></i></div>
+                    <div class="text">Home</div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <div class="icon"><i class="fa-solid fa-book" ></i></div>
+                    <div class="text">Orders</div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <div class="icon"><i class="fa-solid fa-user" ></i></div>
+                    <div class="text">Me</div>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
