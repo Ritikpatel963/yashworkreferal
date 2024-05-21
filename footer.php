8ad1e2bacccba2cf219d052e8fa8296f72ea6ace
unknown <yash190068@gmail.com>
2024-05-21 11:39:30 +0530

    footer fixed

--- footer.php
+++ footer.php
@@ -1,47 +1,49 @@
+<div class="upperfooter d-none d-lg-block" style="padding: 100px;">
+    <div class="upperfooter-content">
+        <div class="upperfooter-logo">
+            <img src="assets/image/logo.png" alt="Company Logo">
+        </div>
+        <div class="upperfooter-text">
+            <p>DS-1, 465, Sec. 10, Ukraine</p>
+            <p>2007 © Indus Power All Rights Reserved</p>
+        </div>
+        <div class="upperfooter-links">
+            <a href="#">Terms of Service</a> |
+            <a href="#">Privacy Policy</a> |
+            <a href="#">Risk Disclosure Agreement</a>
+        </div>
+    </div>
+</div>
+
 <footer class="footer">
-    <div class="upperfooter">
-        <div class="upperfooter-content">
-            <div class="upperfooter-logo">
-                <img src="assets/image/logo.png" alt="Company Logo">
+    <div class="container">
+        <div class="row text-center downfooter">
+            <div class="col text">
+                <a href="index.php">
+                    <div class="icon">
+                        <i class="fa-solid fa-house"></i>
+                        <p style="font-size: 14px;">Home</p>
+                    </div>
+                </a>
             </div>
-            <div class="upperfooter-text">
-                <p>DS-1, 465, Sec. 10, Ukraine</p>
-                <p>2007 © Indus Power All Rights Reserved</p>
+            <div class="col text">
+                <a href="orders.php">
+                    <div class="icon">
+                        <i class="fa-solid fa-book"></i>
+                        <p style="font-size: 14px;">Orders</p>
+                    </div>
+                </a>
             </div>
-            <div class="upperfooter-links">
-                <a href="#">Terms of Service</a> |
-                <a href="#">Privacy Policy</a> |
-                <a href="#">Risk Disclosure Agreement</a>
+            <div class="col text">
+                <a href="account.php">
+                    <div class="icon">
+                        <i class="fa-solid fa-user"></i>
+                        <p style="font-size: 14px;">Me</p>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-
-    <div class="container mt-3">
-        <div class="row text-center downfooter">
-            <div class="row text-center">
-                <div class="col text">
-                    <a href="index.php">
-                        <div class="icon"><i class="fa-solid fa-house"></i></div>
-                        <p style="font-size: 14px;">Home</p>
-                    </a>
-                </div>
-                <div class="col text">
-                    <a href="order.php">
-                        <div class="icon">
-                            <i class="fa-solid fa-book"></i>
-                            <p style="font-size: 14px;">Orders</p>
-                        </div>
-
-                    </a>
-                </div>
-                <div class="col text">
-                    <a href="me.php">
-                        <div class="icon"><i class="fa-solid fa-user"></i></div>
-                        <p style="font-size: 14px;">Me</p>
-                    </a>
-                </div>
-            </div>
-        </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
